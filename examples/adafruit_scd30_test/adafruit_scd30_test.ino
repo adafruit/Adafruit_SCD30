2bd817d458e3718a2ddb7b0b1aa3b75ba5b255fb
lady ada <limor@ladyada.net>
2020-12-20 14:22:22 -0500

    clean up whitespace

--- examples/adafruit_scd30_test/adafruit_scd30_test.ino
+++ examples/adafruit_scd30_test/adafruit_scd30_test.ino
@@ -1,9 +1,9 @@
 // Basic demo for readings from Adafruit SCD30
-#include <Wire.h>
 #include <Adafruit_SCD30.h>
-#include <Adafruit_Sensor.h>
 
 Adafruit_SCD30  scd30;
+
+
 void setup(void) {
   Serial.begin(115200);
   while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
@@ -12,7 +12,6 @@ void setup(void) {
 
   // Try to initialize!
   if (!scd30.begin()) {
-
     Serial.println("Failed to find SCD30 chip");
     while (1) { delay(10); }
   }
@@ -23,25 +22,32 @@ void setup(void) {
   //   Serial.println("Failed to set measurement interval");
   //   while(1){ delay(10);}
   // }
-  Serial.print("Measurement Interval: "); Serial.print(scd30.getMeasurementInterval()); Serial.println(" seconds");
-
-
+  Serial.print("Measurement Interval: "); 
+  Serial.print(scd30.getMeasurementInterval()); 
+  Serial.println(" seconds");
 }
+
 void loop() {
-  if(scd30.dataReady()){
+  if (scd30.dataReady()){
     Serial.println("Data available!");
 
     if (!scd30.read()){ Serial.println("Error reading sensor data"); return; }
 
-    Serial.print("Temperature: ");Serial.print(scd30.temperature);Serial.println(" degrees C");
-    Serial.print("Relative Humidity: ");Serial.print(scd30.relative_humidity);Serial.println(" %");
-    Serial.print("eCO2: ");Serial.print(scd30.eCO2, 3);Serial.println(" ppm");
+    Serial.print("Temperature: ");
+    Serial.print(scd30.temperature);
+    Serial.println(" degrees C");
+    
+    Serial.print("Relative Humidity: ");
+    Serial.print(scd30.relative_humidity);
+    Serial.println(" %");
+    
+    Serial.print("eCO2: ");
+    Serial.print(scd30.eCO2, 3);
+    Serial.println(" ppm");
     Serial.println("");
   } else {
-    Serial.println("No data");
+    //Serial.println("No data");
   }
 
-    delay(100);
+  delay(100);
 }
-
-
